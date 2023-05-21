1c260f0ed09257b4ae24bc85a286d53776cfc7a7
Harithasridhar <82656945+Harithasridhar@users.noreply.github.com>
2023-05-21 11:54:43 +0530

    My Rock Paper Scissors Application

---
+++ rock-paper-scissor/src/main/java/com/game/rockpaperscissor/config/Constants.java
@@ -0,0 +1,9 @@
+package com.game.rockpaperscissor.config;
+
+public class Constants {
+
+    public static String PLAYER_TWO_NAME = "My_Computer (Computer)";
+
+    public static Integer WINNING_SCORE = 3;
+
+}
